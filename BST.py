61b845fddbfb8d5b2cbd66ce27e3c41d917657cc
MacErnest :^) <andreimartillo2000@gmail.com>
2018-02-08 15:30:01 +0800

    added some more things

--- BST.py
+++ BST.py
@@ -6,42 +6,93 @@ class BST:
         self.l_child = None
         self.r_child = None
 
-    def insert(self, array, data=None, index=0):
+    def insert(self, data=None):
         # inserts values from a list into the Binary Search Tree
-        # first condition checks to see if we need to take a value out of the array
-        if data is None and len(array) > index:
-            data = array[index]
         # then we check if the data value is None or if the index is greater than the array's length to end the code
-        if data is None or index > len(array):
+        if data is None:
             pass
         # checks if root node is available (only ever works if you didn't put anything in the initialization)
         elif self.node is None:
             self.node = data
-            self.insert(array, None, index + 1)
         # checks if it is less than the current node's value
         elif data <= self.node:
             # then checks if it is available and then continues with the code recursively
             if self.l_child is None:
                 self.l_child = BST(data)
-                self.insert(array, None, index + 1)
             # else it goes to the left child while making sure that the index is the array length so that the code will end immediately after the data is inputted
             else:
-                self.l_child.insert(array, data, len(array))
-                self.insert(array, None, index + 1)
+                self.l_child.insert(data)
         # checks if it is less than the current node's value
         elif data > self.node:
             # then checks if it is available and then continues with the code recursively
             if self.r_child is None:
                 self.r_child = BST(data)
-                self.insert(array, None, index + 1)
             # else it goes to the right child while making sure that the index is the array length so that the code will end immediately after the data is inputted
             else:
-                self.r_child.insert(array, data, len(array))
-                self.insert(array, None, index + 1)
+                self.r_child.insert(data)
+
+
+def height(_BST):
+    # gains the height of the BST
+    if _BST is not None:
+        if _BST.l_child is not None or _BST.r_child is not None:
+            L = 1 + int(height(_BST.l_child))
+            R = 1 + int(height(_BST.r_child))
+            if L > R:
+                return L
+            else:
+                return R
+            # returns the height whether it be L or R depending on which is larger
+        elif _BST.r_child is None and _BST.l_child is None:
+            return 0
+        else:
+            return 1
+    else:
+        return 0
 
 
 def size(_BT):
     if _BT is not None:
-        return 1 + size(_BT.left) + size(_BT.right)
+        return 1 + size(_BT.l_child) + size(_BT.r_child)
     else:
         return 0
+
+
+def _miniValue(subtree):
+    if subtree.l_child is None:
+        return subtree.node
+    else:
+        return _miniValue(subtree.l_child)
+
+
+def _maxiValue(subtree):
+    if subtree.r_child is None:
+        return subtree.node
+    else:
+        return _maxiValue(subtree.r_child)
+
+
+def search(subtree, value):
+    if subtree is None:
+        return False
+    if subtree.node is value:
+        return True
+    else:
+        return search(subtree.l_child, value) or search(subtree.r_child, value)
+
+
+T = BST(5)
+T.insert(6)
+T.insert(8)
+T.insert(7)
+T.insert(9)
+T.insert(2)
+T.insert(3)
+T.insert(4)
+T.insert(1)
+print(height(T))
+print(_miniValue(T))
+print(_maxiValue(T))
+print(search(T, 5))
+print(search(T, 1))
+print(search(T, 0))
--- Permutation_BST.py
+++ Permutation_BST.py
@@ -1,12 +1,8 @@
 from BST import BST
-from BST import size
 
 
-def Permutation(_BT):
-    if _BT is not None:
-        Permutation(_BT.left)
-        # TODO: Add permutation code here.
-        Permutation(_BT.right)
+def Permutation(_BT, size):
+    # TODO: add code here
 
 
 BT = BST()
